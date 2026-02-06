45e76473d61a1002425e75fd49ec8ff2e252a296
Filip Kjamilov <filipkjamilov@gmail.com>
2026-02-06 10:25:57 +0100

    Initial Commit

---
+++ Costivo/CostivoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CostivoApp.swift
+//  Costivo
+//
+//  Created by Filip Kjamilov on 06.02.26.
+//
+
+import SwiftUI
+
+@main
+struct CostivoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
